omnirom/android_packages_apps_Gallery2

Gallery2: rework photo page layout
fullscreen photo detail page with transparent
action bar and translucent system bars
like in google photos

Change-Id: I5a5e449a5cd43ca98e87583a242c5c3eccaecbcd

src/com/android/gallery3d/app/GalleryActionBar.java
@@ -26,6 +26,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -188,6 +189,7 @@ public class GalleryActionBar implements OnNavigationListener {
 
     public GalleryActionBar(AbstractGalleryActivity activity) {
         mActionBar = activity.getActionBar();
+        mActionBar.setElevation(0);
         mContext = activity.getAndroidContext();
         mActivity = activity;
         mInflater = ((Activity) mActivity).getLayoutInflater();
@@ -438,5 +440,14 @@ public class GalleryActionBar implements OnNavigationListener {
     public void setShareIntents(Intent sharePanoramaIntent, Intent shareIntent) {
         mSharePanoramaIntent = sharePanoramaIntent;
         mShareIntent = shareIntent;
+    }
+
+    public void setSinglePhotoMode(boolean value) {
+        if (value) {
+            mActionBar.setBackgroundDrawable(mActivity.getResources().getDrawable(R.drawable.root_top_bg));
+        } else {
+            mActionBar.setBackgroundDrawable(
+                    new ColorDrawable(mActivity.getResources().getColor(R.color.primary)));
+        }
     }
 }

DanielXS0824/packages_apps_Gallery2

Signed-off-by: mydongistiny <jaysonedson@gmail.com>

src/com/android/gallery3d/app/GalleryActionBar.java
@@ -26,6 +26,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -188,6 +189,7 @@ public class GalleryActionBar implements OnNavigationListener {
 
     public GalleryActionBar(AbstractGalleryActivity activity) {
         mActionBar = activity.getActionBar();
+        mActionBar.setElevation(0);
         mContext = activity.getAndroidContext();
         mActivity = activity;
         mInflater = ((Activity) mActivity).getLayoutInflater();
@@ -438,5 +440,14 @@ public class GalleryActionBar implements OnNavigationListener {
     public void setShareIntents(Intent sharePanoramaIntent, Intent shareIntent) {
         mSharePanoramaIntent = sharePanoramaIntent;
         mShareIntent = shareIntent;
+    }
+
+    public void setTransparentMode(boolean value) {
+        if (value) {
+            mActionBar.setBackgroundDrawable(mActivity.getResources().getDrawable(R.drawable.root_top_bg));
+        } else {
+            mActionBar.setBackgroundDrawable(
+                    new ColorDrawable(mActivity.getResources().getColor(R.color.primary)));
+        }
     }
 }